d6b04e51a5a648ed930cbd5e2014588abdc35237
tomfyfe <tomfyfe85@gmail.com>
2026-02-16 21:20:37 +0000

    add Evetn model

--- cc_registrant_dashboard/registrants/models.py
+++ cc_registrant_dashboard/registrants/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Event(models.Model):
+  name = models.CharField(max_length=255)
+  date = models.DateField(default=date.today)
+  capacity = models.IntegerField()
+
+  def __str__(self):
+    return f"{self.name} - {self.date} - {self.capacity} cap"
+  
+class Registrant(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    company = models.CharField(max_length=255)
+    # TODO 
+    # event (FK), 
+    # guest type (choices)
+    #current status (choices)
+
+# TODO
+# class StatusChange
+# registrant(FK)
+# status,
+# timestamp
