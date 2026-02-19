1c7eb3d543b78d3f9e13a0128d1bd9bfb2aa1636
tomfyfe <tomfyfe85@gmail.com>
2026-02-19 10:16:28 +0000

    add registrant and StatusChange models

--- cc_registrant_dashboard/registrants/models.py
+++ cc_registrant_dashboard/registrants/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import date
+from django.utils import timezone
+
 
 
 class Event(models.Model):
@@ -11,16 +13,46 @@ class Event(models.Model):
     return f"{self.name} - {self.date} - {self.capacity} cap"
   
 class Registrant(models.Model):
+    # guest_type
+    STANDARD = 'STN'
+    CREW = 'CRW'
+    VIP = 'VIP'
+    ARTIST = 'ART'
+    REGISTRANT_TYPE_CHOICES = {
+        STANDARD: "Standard",
+        CREW: 'Crew',
+        VIP: 'VIP',
+        ARTIST: "Artist"
+    }
+
+    # current_status
+    REGISTERED = "REG"
+    CHECKED_IN = "CHK"
+    ENTERED = "ENT"
+    EXITED = "EXT"
+    CANCELLED = "CAN"
+    REGISTRANT_STATUS_CHOICES = {
+        REGISTERED: "Registered",
+        CHECKED_IN: "Checked_in",
+        ENTERED:"Entered",
+        EXITED: "Exited",
+        CANCELLED: "Cancelled",
+    }   
+
     name = models.CharField(max_length=255)
     email = models.EmailField()
     company = models.CharField(max_length=255)
-    # TODO 
-    # event (FK), 
-    # guest type (choices)
-    #current status (choices)
+    event = models.ForeignKey(Event, on_delete=models.PROTECT)
+    guest_type = models.CharField(choices=REGISTRANT_TYPE_CHOICES, default=STANDARD, max_length=3)
+    current_status = models.CharField(choices=REGISTRANT_STATUS_CHOICES, default=REGISTERED, max_length=3)
 
-# TODO
-# class StatusChange
-# registrant(FK)
-# status,
-# timestamp
+
+
+class StatusChange(models.Model):
+    
+    status_choices = Registrant.REGISTRANT_STATUS_CHOICES
+
+    registrant = models.ForeignKey(Registrant, on_delete=models.CASCADE)
+    status = models.CharField(choices=status_choices, default=Registrant.REGISTERED, max_length=3)
+    date = models.DateTimeField(auto_now_add=True)
+
