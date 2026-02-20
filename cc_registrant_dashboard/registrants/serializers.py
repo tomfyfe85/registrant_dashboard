e505dcdad1432795eb9330d1679fe9c8801392f4
tomfyfe <tomfyfe85@gmail.com>
2026-02-20 08:52:07 +0000

    add Registrant Serializer

--- cc_registrant_dashboard/registrants/serializers.py
+++ cc_registrant_dashboard/registrants/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
-from .models import Event
+from .models import Event, Registrant
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 'name', 'date', 'capacity']
 
+class RegistrantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Registrant
+        fields = ['id', 'name', 'email', 'company', 'event', 'guest_type', 'current_status']
--- cc_registrant_dashboard/registrants/views.py
+++ cc_registrant_dashboard/registrants/views.py
@@ -1,10 +1,18 @@
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import EventSerializer
 from .models import Event
 
-@csrf_exempt
+@api_view(["GET"])
 def event_list(request):
+  """
+  List all events
+  """
   event = Event.objects.all()
   serializer = EventSerializer(event, many=True)
-  return JsonResponse(serializer.data, safe=False)
+  return Response(serializer.data)
+
+
+  
+  
