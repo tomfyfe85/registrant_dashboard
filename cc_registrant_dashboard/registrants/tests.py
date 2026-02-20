bd2477c7440399c37fef6dfeb543f32365967972
tomfyfe <tomfyfe85@gmail.com>
2026-02-20 10:34:02 +0000

    add test_create_registrant() passes

--- cc_registrant_dashboard/registrants/tests.py
+++ cc_registrant_dashboard/registrants/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Event
+import pytest
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_create_registrant():
+    test_event1 = Event.objects.create(name='test1', capacity=500)
+    client = APIClient()
+    response = client.post('/registrants/create', {'name': 'test1', 'email': 'test1@gmail.com',
+                                        'company': 'test1_company',
+                                        'event': test_event1.id}, format='json')
+    
+    assert response.status_code == 201
+
+
--- cc_registrant_dashboard/registrants/views.py
+++ cc_registrant_dashboard/registrants/views.py
@@ -1,18 +1,27 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import EventSerializer
+from .serializers import EventSerializer, RegistrantSerializer
 from .models import Event
 
+# Event
 @api_view(["GET"])
 def event_list(request):
-  """
-  List all events
-  """
-  event = Event.objects.all()
-  serializer = EventSerializer(event, many=True)
-  return Response(serializer.data)
+    """
+    List all events
+    """
+    event = Event.objects.all()
+    serializer = EventSerializer(event, many=True)
+    return Response(serializer.data)
 
-
-  
-  
+# Registrant
+@api_view(['POST'])
+def create_registrant(request):
+    """
+    Create a registrant
+    """
+    serializer = RegistrantSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
