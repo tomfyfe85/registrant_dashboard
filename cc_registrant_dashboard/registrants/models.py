3ed956f8202519334b86c95ca9d65921f03cc1ad
tomfyfe <tomfyfe85@gmail.com>
2026-02-23 10:01:19 +0000

    add company_fk field and migrate

--- cc_registrant_dashboard/registrants/models.py
+++ cc_registrant_dashboard/registrants/models.py
@@ -6,17 +6,16 @@ class Event(models.Model):
     name = models.CharField(max_length=255)
     date = models.DateField(default=date.today)
     capacity = models.IntegerField()
-
     def __str__(self):
         return f"Name: {self.name} - Date: {self.date} - Capacity: {self.capacity}"
-
-
+    
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
-
+    
     def __str__(self):
         return f"Company: {self.name}"
+
 
 class Registrant(models.Model):
     # guest_type
@@ -47,13 +46,16 @@ class Registrant(models.Model):
 
     name = models.CharField(max_length=255)
     email = models.EmailField()
-    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+    # company = models.ForeignKey(Company, on_delete=models.PROTECT)
+    company = models.CharField(max_length=255)
+    company_fk = models.ForeignKey(Company, on_delete=models.PROTECT, null=True)
+
     event = models.ForeignKey(Event, on_delete=models.PROTECT)
     guest_type = models.CharField(choices=REGISTRANT_TYPE_CHOICES, default=STANDARD, max_length=3)
     current_status = models.CharField(choices=REGISTRANT_STATUS_CHOICES, default=REGISTERED, max_length=3)
-
-    # def __str__(self):
-    #   return f"Reg_name: {self.name} - Reg_email: {self.email} - Reg_company: {self.company} - Event: {self.event.name} - Reg_type: {self.guest_type} - Reg_status: {self.current_status}"
+    
+    def __str__(self):
+      return f"Reg_name: {self.name} - Reg_email: {self.email} - Reg_company: {self.company} - Event: {self.event.name} - Reg_type: {self.guest_type} - Reg_status: {self.current_status}"
 
 
 class StatusChange(models.Model):
@@ -61,7 +63,7 @@ class StatusChange(models.Model):
     registrant = models.ForeignKey(Registrant, on_delete=models.CASCADE)
     status = models.CharField(choices=status_choices, default=Registrant.REGISTERED, max_length=3)
     date_time = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return f"Reg_id: {self.registrant.id} - Reg_name: {self.registrant.name} - Reg_status: {self.status} - Time_of_status_change: {self.date_time}"
 
