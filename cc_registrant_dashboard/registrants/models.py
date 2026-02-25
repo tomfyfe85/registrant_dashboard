a0983accda9a81d8f37fb31c588acb64da958c99
tomfyfe <tomfyfe85@gmail.com>
2026-02-25 23:43:03 +0000

    chore typo in models.py

--- cc_registrant_dashboard/registrants/models.py
+++ cc_registrant_dashboard/registrants/models.py
@@ -67,4 +67,3 @@ class StatusChange(models.Model):
     def __str__(self):
         return f"Reg_id: {self.registrant.id} - Reg_name: {self.registrant.name} - Reg_status: {self.status} - Time_of_status_change: {self.date_time}"
 
-
