c54eb9a39227fc32f1c54f487668b6ee1a575928
tomfyfe <tomfyfe85@gmail.com>
2026-03-25 08:44:05 +0000

    chore update_status now returns updated pydantic Registrant object

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from database import get_db 
-from cc_registrant_dashboard import Registrant
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
 class StatusUpdate(BaseModel):
+    current_status: str
+
+class Registrant(BaseModel):
+    id: int
+    name: str
+    email: str
+    company_fk_id: int
+    event_id: int
+    guest_type: str
     current_status: str
 
 @app.patch("/registrant/registrant_id/{registrant_id}")
@@ -15,12 +24,17 @@ async def update_status(registrant_id: int, status_update: StatusUpdate):
     status = updates_dict['current_status'] 
    
     conn = get_db()
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute("UPDATE registrants_registrant SET current_status = %s WHERE id = %s RETURNING *", (status, registrant_id))
-    row = cur.fetchall()
+    
+    row = cur.fetchone()
+    print(row)
+
     conn.commit()
     cur.close()
     conn.close()
     updated_registrant = Registrant(**row)
 
     return updated_registrant
+    
+    # todo create and return pydantic Registrants models 
