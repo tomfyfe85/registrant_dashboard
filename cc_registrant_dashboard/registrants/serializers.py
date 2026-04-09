3c9ba5827a338959ffe99885e01bc75b2f2dc262
tomfyfe <tomfyfe85@gmail.com>
2026-04-09 07:38:06 +0100

    add Redis

--- cc_registrant_dashboard/registrants/serializers.py
+++ cc_registrant_dashboard/registrants/serializers.py
@@ -1,17 +1,29 @@
 from rest_framework import serializers
+
 from .models import Event, Registrant, StatusChange
+
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['id', 'name', 'date', 'capacity']
+        fields = ["id", "name", "date", "capacity"]
+
 
 class RegistrantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registrant
-        fields = ['id', 'name', 'email', 'company_fk', 'event', 'guest_type', 'current_status'] 
-        
+        fields = [
+            "id",
+            "name",
+            "email",
+            "company_fk",
+            "event",
+            "guest_type",
+            "current_status",
+        ]
+
+
 class StatusChangeSerializer(serializers.ModelSerializer):
     class Meta:
         model = StatusChange
-        fields = ['id', 'registrant', 'status', 'date_time']
+        fields = ["id", "registrant", "status", "date_time"]
