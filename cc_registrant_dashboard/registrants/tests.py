acfcc34de6be6e0c90721c2ca43e2b5930bc0436
tomfyfe <tomfyfe85@gmail.com>
2026-04-20 18:34:22 +0100

    add testing django pipline2

--- cc_registrant_dashboard/registrants/tests.py
+++ cc_registrant_dashboard/registrants/tests.py
@@ -6,6 +6,8 @@ import pytest
 def test_create_registrant():
     test_event1 = Event.objects.create(name='test1', capacity=500)
     test_company1 = Company.objects.create(name="test_comp_1")
+    print(type(test_company1.id))
+    print(test_event1.id)
     client = APIClient()
     response = client.post('/registrants/create', {'name': 'test1', 'email': 'test1@gmail.com',
                                         'company_fk': test_company1.id,
