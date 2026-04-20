777f71585e128eca6fbf73abf61f98090a6484c2
tomfyfe <tomfyfe85@gmail.com>
2026-04-20 18:10:47 +0100

    add fastAPI and Django pipleline testing

--- cc_registrant_dashboard/registrants/tests.py
+++ cc_registrant_dashboard/registrants/tests.py
@@ -1,13 +1,14 @@
 from rest_framework.test import APIClient
-from .models import Event
+from .models import Event, Company
 import pytest
 
 @pytest.mark.django_db
 def test_create_registrant():
     test_event1 = Event.objects.create(name='test1', capacity=500)
+    test_company1 = Company.objects.create(name="test_comp_1")
     client = APIClient()
     response = client.post('/registrants/create', {'name': 'test1', 'email': 'test1@gmail.com',
-                                        'company_fk': 1,
+                                        'company_fk': test_company1.id,
                                         'event': test_event1.id}, format='json')
     assert response.status_code == 201
 
